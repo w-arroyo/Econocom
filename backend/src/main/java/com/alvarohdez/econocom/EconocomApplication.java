c2b998ecd0595cfaee8eb4a75249bf321b5fff2e
Arroyo <wdonarroyo@gmail.com>
2026-05-08 14:06:38 +0200

    First one

---
+++ backend/src/main/java/com/alvarohdez/econocom/EconocomApplication.java
@@ -0,0 +1,13 @@
+package com.alvarohdez.econocom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EconocomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EconocomApplication.class, args);
+	}
+
+}
